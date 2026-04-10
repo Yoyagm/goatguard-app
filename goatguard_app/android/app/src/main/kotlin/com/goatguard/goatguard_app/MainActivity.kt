b28009a2f6919017b889d198d0e8a6ba1cf1991f
Yoyagm <johan.rc2020@gmail.com>
2026-04-09 19:43:35 -0500

    chore: eliminar directorio goatguard_app obsoleto
    El código de la app Flutter se consolidó en el directorio raíz (lib/).
    El directorio goatguard_app/ era una copia anterior que ya no se mantiene.

--- goatguard_app/android/app/src/main/kotlin/com/goatguard/goatguard_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.goatguard.goatguard_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- goatguard_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:goatguard_app/main.dart';
-
-void main() {
-  testWidgets('App launches', (WidgetTester tester) async {
-    await tester.pumpWidget(const GoatGuardApp());
-    expect(find.text('GOATGUARD'), findsOneWidget);
-  });
-}
